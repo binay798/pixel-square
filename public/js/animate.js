7c73d36c1f18caab0c9b48c6976ad8a64806b2eb
binay <binay6014@gmail.com>
2021-05-21 07:25:44 -0700

    animation added

---
+++ public/js/animate.js
@@ -0,0 +1,38 @@
+let primary = document.querySelectorAll('.cardPrimary');
+let secondary = document.querySelectorAll('.cardSecondary');
+let inp = document.querySelectorAll('.inp')
+
+let duration = 400
+primary.forEach(item => {
+	
+	item.setAttribute('data-sal', 'slide-up');
+	item.setAttribute('data-sal-delay', '100');
+	item.setAttribute('data-sal-duration', duration);
+	item.setAttribute('data-sal-easing', 'easeInSine');
+
+	duration += 100
+})
+secondary.forEach(item => {
+	
+	item.setAttribute('data-sal', 'slide-up');
+	item.setAttribute('data-sal-delay', '100');
+	item.setAttribute('data-sal-duration', duration);
+	item.setAttribute('data-sal-easing', 'easeInSine');
+
+	duration += 100
+})
+
+inp.forEach(item => {
+	
+	item.setAttribute('data-sal', 'slide-up');
+	item.setAttribute('data-sal-delay', '100');
+	item.setAttribute('data-sal-duration', duration);
+	item.setAttribute('data-sal-easing', 'easeInSine');
+
+	duration += 100
+})
+
+sal({
+	thresold: 1,
+	once: false
+})
--- public/js/youtube.js
+++ public/js/youtube.js
@@ -9,7 +9,7 @@ const openModal = (videoSrc) => {
 	modalMain.innerHTML = `
 		<iframe width=100% 
 		height=100% class="iframe" 
-		src=${videoSrc}
+		src=${videoSrc}?controls=0&autoplay=1&enablejsapi=1&title=''&modestbranding=0&fs=0&cc_load_policy=0&iv_load_policy&autohide=0
 		frameborder="0" 
 		allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	`
