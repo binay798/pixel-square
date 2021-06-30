251f46922742578ffaff2186d298c6a7c8081cee
binay798 <binayshrestha798@gmail.com>
2021-06-30 08:16:25 +0545

    js updated

--- public/js/animate.js
+++ public/js/animate.js
@@ -1,35 +1,36 @@
 let primary = document.querySelectorAll('.cardPrimary');
 let secondary = document.querySelectorAll('.cardSecondary');
-let inp = document.querySelectorAll('.inp')
+let inp = document.querySelectorAll('.inp');
 
-let duration = 400
-primary.forEach(item => {
-	
-	item.setAttribute('data-sal', 'slide-up');
-	item.setAttribute('data-sal-delay', '100');
-	item.setAttribute('data-sal-duration', duration);
-	item.setAttribute('data-sal-easing', 'easeInSine');
+let duration = 400;
+primary.forEach((item, id) => {
+  if (id === 0 && window.innerWidth < 450) {
+    console.log('inner width');
+    return 1;
+  }
+  item.setAttribute('data-sal', 'slide-up');
+  item.setAttribute('data-sal-delay', '100');
+  item.setAttribute('data-sal-duration', duration);
+  item.setAttribute('data-sal-easing', 'easeInSine');
 
-	duration += 100
-})
-secondary.forEach(item => {
-	
-	item.setAttribute('data-sal', 'slide-up');
-	item.setAttribute('data-sal-delay', '100');
-	item.setAttribute('data-sal-duration', duration);
-	item.setAttribute('data-sal-easing', 'easeInSine');
+  duration += 100;
+});
+secondary.forEach((item) => {
+  item.setAttribute('data-sal', 'slide-up');
+  item.setAttribute('data-sal-delay', '100');
+  item.setAttribute('data-sal-duration', duration);
+  item.setAttribute('data-sal-easing', 'easeInSine');
 
-	duration += 100
-})
+  duration += 100;
+});
 
-inp.forEach(item => {
-	
-	item.setAttribute('data-sal', 'slide-up');
-	item.setAttribute('data-sal-delay', '100');
-	item.setAttribute('data-sal-duration', duration);
-	item.setAttribute('data-sal-easing', 'easeInSine');
+inp.forEach((item) => {
+  item.setAttribute('data-sal', 'slide-up');
+  item.setAttribute('data-sal-delay', '100');
+  item.setAttribute('data-sal-duration', duration);
+  item.setAttribute('data-sal-easing', 'easeInSine');
 
-	duration += 100
-})
+  duration += 100;
+});
 
-sal()
+sal();
